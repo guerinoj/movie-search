c84b0db68c881046923ed5695999d7793644be68
Jonathan Guerino <guerino.jonathan@gmail.com>
2022-03-07 15:04:15 +0100

    Pagination

--- main.js
+++ main.js
@@ -11,24 +11,54 @@ window.addEventListener("DOMContentLoaded", async function (e) {
     let search = urlParams.get('search');
 
     // S'il y a une page alors je la récupère sinon par défaut je prends la première.
-    page = (urlParams.has("page") ? urlParams.get('page') : 1) 
+    page = (urlParams.has("page") ? urlParams.get('page') : 1)
 
     let response = await movieDOM.getMovies(search, page);
 
     if (response.Response == "True") {
       let movies = response.Search;
 
+      console.log(response);
+      let message = "Le nombre de films trouvés est de " + response.totalResults;
+
+      let div = displayMessage(message, "success");
+      container.appendChild(div);
+
       //Pour chaque film, je génère le DOM
       movies.forEach(movie => {
         container.appendChild(movieDOM.createDOM(movie));
       });
 
+      let pageTotal = calculatePageNumber(response.totalResults);
+      createPagination(pageTotal,"?search="+search);
+
     } else {
-      let div = document.createElement("div");
-      div.classList = "alert alert-danger";
-      div.textContent = response.Error;
+      let div = displayMessage(response.Error, "danger");
       container.appendChild(div);
     }
   }
 
 })
+
+function displayMessage(message, color) {
+  let div = document.createElement("div");
+  div.classList = "alert alert-" + color;
+  div.textContent = message;
+
+  return div;
+}
+
+function calculatePageNumber(nbTotal, nbPerPage = 10) {
+  return Math.ceil(nbTotal / nbPerPage)
+}
+
+function createPagination(nbPage, url) {
+  let ul = document.querySelector(".pagination");
+
+  for (let i = 1; i <= nbPage; i++) {
+    let li = document.createElement("li");
+    li.classList = "page-item";
+    li.innerHTML = `<a class="page-link" href="${url}&page=${i}">${i}</a>`;
+    ul.appendChild(li);
+  }
+}
--- movie.js
+++ movie.js
@@ -8,17 +8,19 @@ class Movie {
       .then(data => {
         return data
       })
-
     return response
   }
 
   createDOM(movie) {
     let movieDOM = document.createElement("div");
-    movieDOM.classList = "col col-md-6 col-lg-4"
+    movieDOM.classList = "col-12 col-md-6 col-lg-4";
+    
+    if (movie.Poster == "N/A")
+      movie.Poster = "https://images.unsplash.com/photo-1559570278-eb8d71d06403?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dGhlYXRlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
 
     movieDOM.innerHTML = `
     <figure>
-      <img class="img-responsive" src ="${movie.Poster}" alt="${movie.Title}">
+      <img class="img-fluid  w-100" src ="${movie.Poster}" alt="${movie.Title}">
       <figcaption>
         <h2>${movie.Title}</h2>
         <p>${movie.Type} - ${movie.Year}</p> 
